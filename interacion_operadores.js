0ac00028813a0236b884a0c3a51fed707db64a36
Francisco Piccirilli <francisco.piccirilli@gmail.com>
2024-01-16 00:44:16 +0100

    sexto commit

---
+++ interacion_operadores.js
@@ -0,0 +1,26 @@
+//Multiplicar
+var resultadoMultiplicacion = 10 * 5;
+console.log("El resultado de la multiplicación es:", resultadoMultiplicacion);
+
+//dividir
+var resultadoDivision = 10 / 2;
+console.log("El resultado de la división es:", resultadoDivision);
+
+//dividir
+var restoDivision = 15 % 9;
+console.log("El resto de la división es:", restoDivision);
+
+//asignar valor para obtener 0
+
+var p = 10;
+var j = 5;
+var o = p + j; 
+console.log("El valor de o es:", o);
+
+//Asigar el valor para obtenter i=50 
+
+var c = 10;
+var m = 5;
+var i = c * m; // Puedes usar el operador de multiplicación para asignar el valor
+console.log("El valor de i es:", i);
+
