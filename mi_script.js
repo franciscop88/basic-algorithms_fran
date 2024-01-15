5502fb662feab6d67cf6f49b6582f1ce13c30c13
Francisco Piccirilli <francisco.piccirilli@gmail.com>
2024-01-15 21:17:53 +0100

    primer commit

---
+++ mi_script.js
@@ -0,0 +1,13 @@
+// Paso 1: Declarar y asignar un héroe favorito
+var myFavoriteHero = "Hulk";
+console.log("Mi héroe favorito es:", myFavoriteHero);
+
+// Paso 2: Declarar y asignar un número a la variable x
+var x = 50;
+console.log("El valor de x es:", x);
+
+// Paso 3: Declarar dos números h e y, luego sumarlos y asignar el resultado a z
+var h = 5;
+var y = 10;
+var z = h + y;
+console.log("La suma de h y y es:", z);
