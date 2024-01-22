9282361f9e95f7b35f1b2ddac941ccc1ba51ea50
Francisco Piccirilli <francisco.piccirilli@gmail.com>
2024-01-22 21:26:55 +0100

    agerego bucles

--- arrays_4.js
+++ arrays_4.js
@@ -1,22 +1,24 @@
-
-//Consigue el valor "HULK" del array de avengers y muéstralo por consola
+// Consigue el valor "HULK" del array de avengers y muéstralo por consola
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 console.log(avengers[0]); // Muestra "HULK"
 
+// Cambia el primer elemento de avengers a "IRONMAN"
 avengers[0] = "IRONMAN";
-console.log(avengers); 
+console.log(avengers);
 
-const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
-console.log(avengers.length); // Muestra 3
+// Imprime el número de elementos en el array
+let avengersArray = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengersArray.length); // Muestra 3
 
+// Añade 2 elementos al array: "Morty" y "Summer"
 const rickAndMortyCharacters1 = ["Rick", "Beth", "Jerry"];
 rickAndMortyCharacters1.push("Morty", "Summer");
 console.log(rickAndMortyCharacters1[rickAndMortyCharacters1.length - 1]); // Muestra "Summer"
 
 // Elimina el último elemento del array y muestra el primero y el último por consola
-
 let rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 const ultimoPersonaje = rickAndMortyCharacters2.pop();
+
 console.log(rickAndMortyCharacters2[0]); // Muestra "Rick"
 console.log(ultimoPersonaje); // Muestra "Lapiz Lopez"
 
